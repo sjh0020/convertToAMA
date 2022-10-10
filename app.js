acf08783520707977ca2267a76820c63ead42cc2
sjh <1877685805@qq.com>
2022-10-10 23:58:39 +0800

    update

--- app.js
+++ app.js
@@ -8,20 +8,32 @@
 //     authorNames = authorNames.replaceAll("  ", " ");
 //     // alert(authorNames);
 // }
-
 document.querySelector('#generate').onclick = generateAMA;
-document.querySelector('#copy').onclick = copyText;
-
-function generateAMA() {
-    let copyTxt = document.querySelector('#copy')
-    copyTxt.classList.remove("disable")
+// document.querySelector('#copy').onclick = copyText;
+document.querySelector('#copy').onclick = async () => {
+    // let copyTxt =document.getElementById('result').innerHTML;
+    // console.log(copyTxt);
+    await navigator.clipboard.write(new ClipboardItem({'text/html': new Blob([this.document.getElementById('result').innerHTML],{type: 'text/html',}),}));
 }
 
+function generateAMA() {
+    let btn = document.querySelector('#copy')
+    btn.classList.remove("disable")
+}
+/*
 function copyText() {
-    let text = document.getElementById("result").innerHTML;
-    console.log(text);
-    let input = document.getElementById("input");
-    input.value = text;
-    input.select();//选中文本
-    document.execCommand("copy");
+    let range = document.createRange();
+    const aa = document.getElementById('result');  // 这里是你要选择复制的文本dom节点
+    window.getSelection().removeAllRanges();
+    range.selectNode(aa);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+}
+*/
+async function copyText() {
+    // let copyTxt = 'Makar AB, McMartin KE, Palese M, Tephly TR. Formate assay in body fluids: application in methanol poisoning. <i>Biochem Med</i>. 1975;13(2):117-126. doi:10.1016/0006-2944(75)90147-7';
+    let copyTxt =document.getElementById('result').innerHTML;
+    console.log(copyTxt);
+    await navigator.clipboard.write(new ClipboardItem({'text/html': new Blob([this.copyTxt],{type: 'text/html',}),}));
 }
