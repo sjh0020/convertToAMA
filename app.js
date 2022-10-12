3eec298c20cd4dc083d6bfdf1f0ddb4206abb5e1
1977685805@qq.com <1977685805@qq.com>
2022-10-12 14:57:32 +0800

    update the app.js

--- app.js
+++ app.js
@@ -26,29 +26,15 @@ function copyText() {
     window.getSelection().removeAllRanges();
 }
 */
-// clipboard.write方法
-
+/* // clipboard.write方法
+navigator.clipboard.write([new ClipboardItem({'text/html': new Blob([document.getElementById('result').innerHTML,], {type: 'text/html',}),}),]);
+*/
 function copyText() {
-  try {
-    navigator.permissions.query({name: 'clipboard-write'}).then(status => {
-      console.log(status.state);
-      if (status.state === 'denied') {
-        throw new Error('Not allow to write the clipboard!');
-      } else if (status.state === "granted") {
-          navigator.clipboard.write([new ClipboardItem({'text/html': new Blob([document.getElementById('result').innerHTML,], {type: 'text/html',}),}),]);
-        }});
-  }
-  catch(err) {
-    console.log(err.message);
     let range = document.createRange();
-    const aa = document.getElementById('result');  // 这里是你要选择复制的文本dom节点
     window.getSelection().removeAllRanges();
-    range.selectNode(aa);
+    range.selectNode(document.getElementById('result'));// 这里是你要选择复制的文本dom节点
     window.getSelection().addRange(range);
     document.execCommand('copy');
     window.getSelection().removeAllRanges();
-  }
-  finally {
     console.log('Copiled!');
-  }
 }
